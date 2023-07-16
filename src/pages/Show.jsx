8c0ecabe60d7e1d5c9fb25ba35e283954893797c
Ashish <kharbashish2121@gmail.com>
2023-07-16 22:54:10 +0530

    data fetching with libraries: React query

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,38 +1,24 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getShowById } from "../api/tvmaze"; 
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getShowById } from '../api/tvmaze';
 
-const useShowById = (showId) => {
-    const [showData,setShowData] = useState(null);
-    const [showError,setShowError] = useState(null);
-
-    useEffect(()=>{
-        async function fetchData(){
-            try{
-                const data = await getShowById(showId);
-                setShowData(data);
-            }catch(err){
-                setShowError(err);
-            }
-        }
-        fetchData();
-    },[showId])
-    return {showData,showError}
-}
 const Show = () => {
-    const {showId} = useParams();
-    const {showData,showError} = useShowById(showId)
-    if(showError)
-    {
-        return <div>We have an error: {showError.message}</div>
+    const { showId } = useParams();
+
+    const { data: showData, error: showError } = useQuery({
+        queryKey: ['show', showId],
+        queryFn: () => getShowById(showId),
+    });
+
+    if (showError) {
+        return <div>We have an error: {showError.message}</div>;
     }
 
-    if(showData)
-    {
-        return <div>Got show data: {showData.name}</div>
+    if (showData) {
+        return <div>Got show data: {showData.name}</div>;
     }
 
-    return <div>Data is loading...</div>
-}
+    return <div>Data is loading...</div>;
+};
 
 export default Show;
