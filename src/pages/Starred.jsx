d773a82a41c7fa5a5141024e47ed755a9f16d5b3
Ashish <kharbashish2121@gmail.com>
2023-07-17 17:27:00 +0530

    fetch shows from the API on starred page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,9 +1,30 @@
-import { useStarredShows } from "../lib/useStarredShows";
+import { getShowByIds } from '../api/tvmaze';
+import { useStarredShows } from '../lib/useStarredShows';
+import { useQuery } from '@tanstack/react-query';
+import ShowGrid from '../components/shows/ShowGrid'
 
 const Starred = () => {
-    const [starredShows] = useStarredShows();
+    const [starredShowsIds] = useStarredShows(); // we only destructured starredShowIds from useStarredShows return value
 
-    return <div>Starred page, starred {starredShows.length}</div>;
+    const { data: starredShows, error: starredShowsError } = useQuery({
+        queryKey: ['starred', starredShowsIds],
+        queryFn: () => 
+            getShowByIds(starredShowsIds).then(result => 
+                result.map(show => ({show}))
+                ),
+        refetchOnWindowFocus: false,
+    });
+    if(starredShows?.length >0){
+        return <ShowGrid shows={starredShows}/>
+    }
+    if(starredShows?.length === 0){
+        return <div>No shows are starred.</div>
+    }
+    if(starredShowsError){
+        return <div>Error occured: {starredShowsError.message}</div>
+    }
+
+    return <div>Shows are loading....</div>;
 };
 
 export default Starred;
